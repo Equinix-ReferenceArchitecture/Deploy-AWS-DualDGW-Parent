8667c0f7a646346edf60841237f304564792a702
vipin2060 <vipinvikraman86@gmail.com>
2025-06-01 22:44:49 +0800

    Create outputs.tf
    Uploading outputs file

---
+++ outputs.tf
@@ -0,0 +1,17 @@
+output "Dx_Gateway_ID_01" {
+  value = module.Deploy-DualRegion-DGW-01.Dx_Gateway_ID
+}
+
+
+output "Dx_Gateway_name_01" {
+  value = module.Deploy-DualRegion-DGW-01.Dx_Gateway_name
+}
+
+output "Dx_Gateway_ID_02" {
+  value = module.Deploy-DualRegion-DGW-02.Dx_Gateway_ID
+}
+
+
+output "Dx_Gateway_name_02" {
+  value = module.Deploy-DualRegion-DGW-02.Dx_Gateway_name
+}
